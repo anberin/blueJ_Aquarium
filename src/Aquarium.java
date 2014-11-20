551008d8d47bd50b416cadf37b833c0841e86dcc
Misha Prasolov <mishanet123@gmail.com>
2014-11-19 21:38:45 -0600

    Cleaning up da code

--- src/Aquarium.java
+++ src/Aquarium.java
@@ -1,11 +1,9 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Arrays;
+import java.util.Calendar;
 
+@SuppressWarnings("UnusedAssignment")
 class Aquarium {
     public static final String DATE_FORMAT_NOW = "yyyy-MM-dd(HH-mm)";
 
@@ -16,63 +14,82 @@ class Aquarium {
     }
 
     public static void main(String[] args) {
-        int numberOfFish = 50, randomAge, randomType, randomAlive, randomColor;
+        File dir = new File("output");
+        dir.mkdir();
+        int numberOfFish = 50, randomAge, randomType, randomColor;
         String fishType;
         String fileName;
         char fishColor;
-        boolean fishAlive = false;
+        boolean fishAlive;
         PrintWriter print = null;
         fileName = now() + "output.txt";
         Fish[] tank = new Fish[numberOfFish];
 
         for (int i = 0; i < numberOfFish; i++) {
             randomType = (int) (Math.random() * 5) + 1;
-            if (randomType == 1) fishType = "minnow";
-            if (randomType == 2) fishType = "bass";
-            if (randomType == 3) fishType = "gold";
-            if (randomType == 4) fishType = "beta";
-            else fishType = "pike";
-
-            randomAge = (int) (Math.random() * 10) + 0;
-            randomAlive = (int) (Math.random() * 2) + 1;
-            if (randomAlive == 1) fishAlive = true;
-
+            if (randomType == 1) {
+                fishType = "minnow";
+            } else if (randomType == 2) {
+                fishType = "bass";
+            } else if (randomType == 3) {
+                fishType = "gold";
+            } else if (randomType == 4) {
+                fishType = "beta";
+            } else {
+                fishType = "pike";
+            }
+            randomAge = (int) (Math.random() * 10);
+            fishAlive = (Math.random() < 0.5);
             randomColor = (int) (Math.random() * 6) + 1;
-            if (randomColor == 1) fishColor = 'r';
-            else if (randomColor == 2) fishColor = 'b';
-            else if (randomColor == 3) fishColor = 'p';
-            else if (randomColor == 4) fishColor = 'y';
-            else if (randomColor == 5) fishColor = 'o';
-            else fishColor = 'g';
-
+            if (randomColor == 1) {
+                fishColor = 'r';
+            } else if (randomColor == 2) {
+                fishColor = 'b';
+            } else if (randomColor == 3) {
+                fishColor = 'p';
+            } else if (randomColor == 4) {
+                fishColor = 'y';
+            } else if (randomColor == 5) {
+                fishColor = 'o';
+            } else {
+                fishColor = 'g';
+            }
             tank[i] = new Fish(fishType, randomAge, fishAlive, fishColor);
         }
-
-        //Arrays.sort(tank);
 
         try {
             print = new PrintWriter(new BufferedWriter(new FileWriter("output/" + fileName, true)));
         } catch (IOException iox) {
             System.out.println("Problem writing " + fileName);
         }
+
         print.println("#Fish Population in Order of Generation#");
         print.println("Fish Type" + "\t\t" + "Fish Age" + "\t\t" + "Fish Alive?" + "\t\t" + "Fish Color");
         print.println("=========" + "\t\t" + "========" + "\t\t" + "===========" + "\t\t" + "==========");
         for (int i = 0; i < numberOfFish; i++) {
             print.println(tank[i].getType() + "\t\t\t" + tank[i].getAge() + "\t\t\t\t" + tank[i].isAlive() + "\t\t\t" + tank[i].getColor());
         }
+
         print.println("==========================================================");
         Arrays.sort(tank);
+
         print.println("#Fish Population in Order of Age#");
         print.println("Fish Type" + "\t\t" + "Fish Age" + "\t\t" + "Fish Alive?" + "\t\t" + "Fish Color");
         print.println("=========" + "\t\t" + "========" + "\t\t" + "===========" + "\t\t" + "==========");
         for (int i = 0; i < numberOfFish; i++) {
             print.println(tank[i].getType() + "\t\t\t" + tank[i].getAge() + "\t\t\t\t" + tank[i].isAlive() + "\t\t\t" + tank[i].getColor());
         }
-        print.println("==========================================================");
-        print.println();
-        print.close();
 
+        print.println("==========================================================");
+        
+        print.println("#Statistics#");
+        print.println("Average Population Age: ");
+        print.println("Number of Minnows: ");
+        print.println("Number of Bass: ");
+        print.println("Number of Goldfish: ");
+        print.println("Number of Betas: ");
+        print.println("Number of Pike: ");
+        print.close();
     }
 }
 
@@ -124,43 +141,7 @@ class Fish implements Comparable<Fish> {
     public int compareTo(Fish compareFish) {
 
         int compareAge = ((Fish) compareFish).getAge();
-
-        //ascending order
         return this.age - compareAge;
 
-        //descending order
-        //return compareQuantity - this.quantity;
-
     }
-
-//    public void bubbleSort(int[] arr) {
-//        boolean swapped = true;
-//        int j = 0;
-//        int tmp;
-//        while (swapped) {
-//            swapped = false;
-//            j++;
-//            for (int i = 0; i < arr.length - j; i++) {
-//                if (arr[i] > arr[i + 1]) {
-//                    tmp = arr[i];
-//                    arr[i] = arr[i + 1];
-//                    arr[i + 1] = tmp;
-//                    swapped = true;
-//                }
-//            }
-//        }
-//    }
-//
-//    public static double calcAverage(int[] people) {
-//        double sum = 0;
-//        for (int i = 0; i < people.length; i++) {
-//            sum = sum + people[i];
-//        }
-//        double result = sum / people.length;
-//        System.out.println(result);
-//        return result;
-//    }
-
-//    int[] myPeople = {1,2,3,4,5,6,7,8};
-//    double myPeopleAverage = calcAverage(myPeople);
 }
